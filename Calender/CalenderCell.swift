5f96c974b25a344f1fc70d4233736a686351cc1f
Shehzad khan <khan415072@gmail.com>
2022-02-04 16:48:12 +0530

    Simple Calender Application for iOS devices.

---
+++ Calender/CalenderCell.swift
@@ -0,0 +1,13 @@
+//
+//  CalenderCell.swift
+//  Calender
+//
+//  Created by Sunny Shehzad on 02/02/22.
+//
+
+import UIKit
+
+class CalenderCell: UICollectionViewCell
+{
+    @IBOutlet weak var dayOfMonth: UILabel!
+}
